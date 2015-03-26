205f1570902e3ef1cefa3ca73c0c992c99594b0d
Josh Berryman <the.real.josh.berryman@gmail.com>
2015-03-26 17:19:19 +0100

    updated usage message

--- src/usage.h
+++ src/usage.h
@@ -9,7 +9,8 @@ char const usage[] =
 "  -nsteps       <n> (int)    number of tumbling orientations to sample (from a Fibbonacci grid).\n"
 "\n"
 "  Optionally:\n"
-"  -seed    <s> (int)    to ensure repeatability by seeding the random number generator.\n"
-"  -verbose <v>          to engage talkative mode.\n"
-"  -log     <l> (string) to redirect output to a logfile\n";
+"  -seed     <s> (int)    to ensure repeatability by seeding the random number generator.\n"
+"  -norients <o> (int)    number of random grid orientations to sample over (default 10)\n"
+"  -verbose  <v>          to engage talkative mode.\n"
+"  -log      <l> (string) to redirect output to a logfile\n";
 
