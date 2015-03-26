556abe5d493d92933e68e1b5f0827bf03303815a
Josh Berryman <the.real.josh.berryman@gmail.com>
2015-03-26 16:25:23 +0100

    Modified test script to use Fibonacci grid version of code

--- test/testC.bash
+++ test/testC.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-../crossArea -a 20 -g 2.5 -i miniTest.pqr -r ../lib/sc_radii.lib -v > testC.log 2>testC.elog 
+../src/crossArea -n 2000 -g 2.5 -i miniTest.pqr -r ../lib/sc_radii.lib -v > testC.log 2>testC.elog 
 
 
-#../c/crossArea 20 2.5 miniTest.pqr ../lib/sc_radii.lib  > testC.log
+../src/crossArea_old -a 80 -g 2.5 -i miniTest.pqr -r ../lib/sc_radii.lib -v  > testC_old.log
 
 
